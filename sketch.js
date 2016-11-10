2514fae8428993f640d6872dc783a85b742bd84c
benjaminsegars <78641@bwls.net>
2016-11-10 09:18:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-function setup(Stroke(25,25,0)) {
-createCanvas(800,800)
-background(217,50,0)  
+function setup(Stroke(25,25,0);
+) {
+createCanvas(800,1000);
+background(217,50,0);
 }
 
 function draw() {
-ellipse(mouseX,mouseY,100,100)
-fill(275,102,0)
+ellipse(mouseX,mouseY,100,100);
+fill(275,102,0);
   
 }
